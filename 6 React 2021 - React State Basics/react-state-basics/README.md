ee6796751296f68d3c4496bae1d6e5a0467cff7f
bhargov <Bhargovjyotidasjyoti@gmail.com>
2021-12-07 14:34:45 +0530

    added

---
+++ 6 React 2021 - React State Basics/react-state-basics/README.md
@@ -0,0 +1,3 @@
+# State
+1. Clicker.js component is a class component contain state.
+2. Clicker component getting props value from parent App.js component.
---
+++ 6 React 2021 - React State Basics/react-state-basics/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Clicker from './Clicker';
+
+function App() {
+  return (
+    <div className="App">
+      <Clicker greet="Welcome"/>
+    </div>
+  );
+}
+
+export default App;
