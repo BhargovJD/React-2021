7235edc5ed980791fdaa62faab417123a79fa0b4
bhargov <Bhargovjyotidasjyoti@gmail.com>
2021-12-10 14:55:29 +0530

    added

--- 9 React 2021 - State Exercises/1 Flip Coin/flip-coin/src/Flipcoin.js
+++ 9 React 2021 - State Exercises/1 Flip Coin/flip-coin/src/Flipcoin.js
@@ -9,25 +9,61 @@ export default class Flipcoin extends Component {
         super(props);
         this.state = {
             side: HEAD,
-            alt: "head-side"
+            alt: "head-side",
+            headScore: 0,
+            tailScore: 0,
         };
 
-        this.changeCoin = this.changeCoin.bind(this);
+        this.randomChange = this.randomChange.bind(this);
+        this.incrementHeadScore = this.incrementHeadScore.bind(this);
+        this.incrementTailScore = this.incrementTailScore.bind(this);
 
     }
 
-    changeCoin(e) {
-        if (this.state.side === HEAD) {
+
+    incrementTailScore(curState) {
+        return {
+            tailScore: curState.tailScore + 1
+        };
+    }
+
+    incrementHeadScore(curState) {
+        return {
+            headScore: curState.headScore + 1
+        };
+    }
+
+
+    randomChange() {
+        var n = Math.floor(Math.random() * 2);
+        // console.log(n)
+
+        // tail
+        if (n === 0) {
+            console.log('This is 0: Tail');
             this.setState({ side: TAIL });
             this.setState({ alt: "tail-side" });
+
+            this.setState(this.incrementTailScore);
+
+
+
         }
 
-        if (this.state.side === TAIL) {
+        // head
+        if (n === 1) {
+            console.log('This is 1: Head');
             this.setState({ side: HEAD });
             this.setState({ alt: "head-side" });
-        }
 
+            this.setState(this.incrementHeadScore);
+        }
     }
+
+
+
+
+
     render() {
 
         console.log(this.state.side);
@@ -35,7 +71,10 @@ export default class Flipcoin extends Component {
             <div>
                 <img className="Coin" src={this.state.side} alt={this.state.alt} />
                 <br />
-                <button onClick={this.changeCoin}>Flip Coin</button>
+                <button onClick={this.randomChange}>Flip Coin</button>
+                <br />
+                <h3>Head Score: {this.state.headScore}</h3>
+                <h3>Tail Score: {this.state.tailScore}</h3>
             </div>
         )
     }
