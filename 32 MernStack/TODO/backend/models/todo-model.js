d532e09c730eb93e38d0a9807c5855f66d9b38cd
bhargov <Bhargovjyotidasjyoti@gmail.com>
2022-02-08 14:03:00 +0530

    added

---
+++ 32 MernStack/TODO/backend/models/todo-model.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const todoSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 100,
+  },
+
+  author: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 100,
+  },
+
+  a_id: {
+    type: String,
+  },
+
+  isComplete: {
+    type: Boolean,
+  },
+
+  date: {
+    type: Date,
+    default: new Date(),
+  },
+});
+
+const Todo = mongoose.model("Todo", todoSchema);
+
+exports.Todo = Todo;
+// module.exports = Todo
+// export default Todo
